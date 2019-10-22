1078769724d8fcda0b1dc462a3de1942df676d8a
Wellington Rodrigues <damac@Wellingtons-MacBook-Pro.local>
2019-10-22 18:01:30 -0400

    changed instance ID

--- prod/match.py
+++ prod/match.py
@@ -41,7 +41,7 @@ def send_request (uid, version, isTest=True):
     resp = client.send_command(
         DocumentName="AWS-RunShellScript", # One of AWS' preconfigured documents
         Parameters={'commands': commands},
-        InstanceIds=['i-08748d9ff0c114b6f']
+        InstanceIds=['i-0ef37d38dbbcc50d3']
     )
     log('request sent')
     log(resp)
