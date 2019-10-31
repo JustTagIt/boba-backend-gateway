1b7a466fa3260e9191f45d2dbdb80ff2426cb4aa
Connor-DDS <Connor@friends.dds.mil>
2019-10-31 10:15:55 -0400

    removed fetch batch function

--- prod/sync_down.py
+++ prod/sync_down.py
@@ -13,42 +13,13 @@ class DecimalEncoder(json.JSONEncoder):
             else:
                 return int(o)
         return super(DecimalEncoder, self).default(o)
-
-# scans dynamodb and returns dict of UIDs 
-# Select UID from BOBA-Encounters where UID > uid and IMEI =/= imei and UID
-# between UID and UID+10
-def fetch_batch(uid, imei):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('BOBA-Encounters')
-    
-    fe = Attr('UID').between(uid+1, uid+10) & Attr('IMEI').ne(imei) & Attr('Flag').eq("Used")
-    pe = 'UID'
-    
-    response = table.scan(
-            FilterExpression = fe,
-            ProjectionExpression = pe,
-            )
-              
-    data = response['Items']
-    
-    while 'LastEvaluatedKey' in response:
-        response = table.scan(
-            ExclusiveStartKey = response['LastEvaluatedKey'],
-            FilterExpression = fe,
-            ProjectionExpression = pe)
-        data.extend(response['Items'])
-    
-    ids = ""
-    for i in range(0,len(data)):
-        ids += str(data[i]["UID"])+","
-        
-    return ids
     
 
 def get_max_id_issued():
-    dynamodb = boto3.resource("dynamodb")
+     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table("BOBA-IDCounter")
     response = table.get_item(Key={'ID': '0'})
     UID = int(response['Item']['UID'])-1
+    print("MAX ID RESULT FROM DYNAMO: " + str(UID))
 
     return UID
--- test/sync_down.py
+++ test/sync_down.py
@@ -14,44 +14,11 @@ class DecimalEncoder(json.JSONEncoder):
                 return int(o)
         return super(DecimalEncoder, self).default(o)
 
-# scans dynamodb and returns dict of UIDs 
-# Select UID from BOBA-Encounters where UID > uid and IMEI =/= imei and UID
-# between UID and UID+10
-def fetch_batch(uid, imei, data_epoch_token):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('BOBA-Encounters-Test')
-    fe = Attr('UID').between(uid+1, uid+10) & Attr('Flag').eq("Used")
-    pe = 'UID'
-    
-    response = table.scan(
-            FilterExpression = fe,
-            ProjectionExpression = pe,
-            )
-              
-    data = response['Items']
-    
-    while 'LastEvaluatedKey' in response:
-        response = table.scan(
-            ExclusiveStartKey = response['LastEvaluatedKey'],
-            FilterExpression = fe,
-            ProjectionExpression = pe)
-        data.extend(response['Items'])
-    
-    ids = ""
-    for i in range(0,len(data)):
-        ids += str(data[i]["UID"])+","
-        
-    return ids
-    
 
 def get_max_id_issued():
-    print("MAX FUNCTION START")
     dynamodb = boto3.resource("dynamodb")
-    print("MAX FUNCTION START STEP 2")
     table = dynamodb.Table("BOBA-IDCounter-Test")
-    print("MAX FUNCTION START STEP 3")
     response = table.get_item(Key={'ID': '0'})
-    print("MAX FUNCTION START STEP 4")
     UID = int(response['Item']['UID'])-1
     print("MAX ID RESULT FROM DYNAMO: " + str(UID))
     return UID
